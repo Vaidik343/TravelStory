56ea92c3cad42a984bf69fe1da545a906bf5321e
Vaidik343 <vaidik.bbcspl@gmail.com>
2026-03-30 18:24:16 +0530

    feat: Add expo-sqlite dependency and update package versions for compatibility

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,14 @@ import { Redirect } from "expo-router";
 import { useAuth } from "../context/AuthContext";
 
 export default function Index() {
-  const { user, loading } = useAuth();
+  const { isAuthenticated, loading } = useAuth();
 
-  // Optional: prevent flicker while auth loads
+  // Prevent flicker while auth loads
   if (loading) return null;
 
-  if (user) {
-    return <Redirect href="/(tabs)" />;
+  if (isAuthenticated) {
+    return <Redirect href={"/(tabs)" as any} />;
   }
 
-  return <Redirect href="/(auth)" />;
+  return <Redirect href={"/(auth)" as any} />;
 }
